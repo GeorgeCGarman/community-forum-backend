63b1d2bfb61903de60040603e5f3686121c14ff4
George Garman <georgecolingarman.uk@gmail.com>
2022-09-15 15:06:36 +0100

    modified index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
 const dotenv = require("dotenv")
-const middleware = require('./middleware/index')
-const ejs = require('ejs')
-const bodyParser = require('body-parser')
+const middleware = require("./middleware/index")
+const ejs = require("ejs")
+const bodyParser = require("body-parser")
 
 const questionRouter = require("./routes/questionRoutes")
 const commentRouter = require("./routes/commentRoutes")
@@ -13,28 +13,36 @@ const upvoteRouter = require("./routes/upvoteRouter")
 const app = express()
 dotenv.config()
 
+// import { createClient } from "redis"
+
+// const client = createClient()
+
+// client.on("error", (err) => console.log("Redis Client Error", err))
+
+// await client.connect()
+
 //use this for Production
 // const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 //local
 // MONGO_URL=``
 // const mongoURL = process.env.MONGO_URL
 const mongoURL = `mongodb://localhost:27017/forum-db`
-const connectWithRetry = () =>{
-mongoose
+const connectWithRetry = () => {
+  mongoose
     .connect(mongoURL)
-    .then(()=> console.log("Successfully connected to DB"))
-    .catch((e)=> {
-        console.log(e)
-        setTimeout(connectWithRetry, 5000)
+    .then(() => console.log("Successfully connected to DB"))
+    .catch((e) => {
+      console.log(e)
+      setTimeout(connectWithRetry, 5000)
     })
 }
 connectWithRetry()
 
-app.enable("trust proxy");
+app.enable("trust proxy")
 app.use(cors({}))
 app.use(express.json())
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set("view engine", "ejs")
+app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: false }))
 
 //Middleware
@@ -44,7 +52,6 @@ app.use("/questions", questionRouter)
 app.use("/comments", commentRouter)
 app.use("/upvotes", upvoteRouter)
 
+const port = process.env.PORT || 3000
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, ()=> console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`))
