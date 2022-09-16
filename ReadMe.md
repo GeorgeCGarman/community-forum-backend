42ef20b53939a70be4928886195900b9c30ed051
George Garman <georgecolingarman.uk@gmail.com>
2022-09-16 10:19:40 +0100

    add readme

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# community-forum backend
+
+This is a backend for the community forum project, where users can create questions and leave comments, and also upvote these comments.
+
+## Start the server
+
+```
+node index.js
+```
--- models/commentModel.js
+++ models/commentModel.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const commentSchema = new mongoose.Schema({
-  user_id: {type: String, required: true},
-  question_id: {type: String, required: true},
-  date_created: {type: Date, default: Date.now()},
-  comment: {type: String, required: true},
-  upvotes: {type: Number}
+  user_id: { type: String, required: true },
+  question_id: { type: String, required: true },
+  date_created: { type: Date, default: Date.now() },
+  comment: { type: String, required: true },
+  upvotes: { type: Number },
 })
 
 const Comment = mongoose.model("Comment", commentSchema)
